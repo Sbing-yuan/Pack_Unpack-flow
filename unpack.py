86d4d444b8c0c41fe8d63f70f79313b21470cebc
Sbing-yuan <62290987+Sbing-yuan@users.noreply.github.com>
2024-03-15 18:20:21 +0800

    Create unpack.py

---
+++ unpack.py
@@ -0,0 +1,39 @@
+#! python3.7
+# =================================================================================================
+# Rivision History:
+# Date          By              Ext.    Version         Revision History
+# -------------------------------------------------------------------------------------------------
+# 2024.03.06    John She        xx    First           Unpack WorkStation file pack2pc.tar.gz.uu
+# =================================================================================================
+import uu
+import os
+import tarfile
+from datetime import datetime
+import time
+
+start_T = time.time()
+date = datetime.now()
+date_format = date.strftime("%y%m%d_%H%M")
+# print(date_format)
+
+decode_file = "pack2pc.tar.gz"
+
+# remove xx.tar.gz which will conlflict old uu.decode result
+try:
+    os.remove(decode_file)
+except:
+    print("[Warn] Not able to remove old decode file")
+
+uu.decode(decode_file + ".uu")
+
+# open file
+file = tarfile.open(decode_file)
+
+# extracting file
+file.extractall("./" + date_format)
+
+file.close()
+end_T = time.time()
+
+print("End of unpack.py, time usage: %s seconds" % (end_T - start_T))
+input("Press Enter to continue ...")
